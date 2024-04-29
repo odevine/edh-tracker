2e82b25c48437eb2b9f61420a61a1cae39c25ed8
Owen Devine <owen@devine.dev>
2024-04-28 20:03:36 -0400

    Fix builds part 2

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import { StrictMode } from "react";
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import { Authenticator } from "@aws-amplify/ui-react";
 
 import { App } from "./App";
 
 import { ThemeProvider } from "@/Context";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider>
       <Authenticator.Provider>
