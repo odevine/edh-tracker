5f27206089d22f2ff41ea8ec597f8f40d467df34
Owen Devine <owen@devine.dev>
2024-05-16 09:29:07 -0400

    Remove test buttons on homepage

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -1,8 +1,6 @@
-import { useApp } from "@/Context";
-import { Button, Stack, Typography } from "@mui/material";
+import { Stack, Typography } from "@mui/material";
 
 export const HomePage = (): JSX.Element => {
-  const { addAppMessage } = useApp();
   return (
     <Stack
       sx={{ height: "100%", width: "100%", textAlign: "center" }}
@@ -12,50 +10,6 @@ export const HomePage = (): JSX.Element => {
     >
       <Typography variant="h2">home page</Typography>
       <Typography variant="caption">(coming soon)</Typography>
-      <Button
-        color="success"
-        onClick={() =>
-          addAppMessage({
-            content: "test words",
-            severity: "success",
-          })
-        }
-      >
-        success
-      </Button>
-      <Button
-        color="info"
-        onClick={() =>
-          addAppMessage({
-            content: "test words",
-            severity: "info",
-          })
-        }
-      >
-        info
-      </Button>
-      <Button
-        color="warning"
-        onClick={() =>
-          addAppMessage({
-            content: "test words",
-            severity: "warning",
-          })
-        }
-      >
-        warning
-      </Button>
-      <Button
-        color="error"
-        onClick={() =>
-          addAppMessage({
-            content: "test words",
-            severity: "error",
-          })
-        }
-      >
-        error
-      </Button>
     </Stack>
   );
 };
