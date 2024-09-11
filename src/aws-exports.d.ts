9146beba8efb3827d8f69daeb54300ed3e30936a
Owen Devine <owen@devine.dev>
2024-09-11 09:25:20 -0400

    chore: add types file for aws-exports

---
+++ src/aws-exports.d.ts
@@ -0,0 +1,10 @@
+declare const awsmobile: {
+  aws_project_region?: string;
+  aws_cognito_identity_pool_id?: string;
+  aws_cognito_region?: string;
+  aws_user_pools_id?: string;
+  aws_user_pools_web_client_id?: string;
+  oauth?: object;
+};
+
+export default awsmobile;
