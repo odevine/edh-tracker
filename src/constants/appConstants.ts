21ccc2de845a10592ae7386cc81c3d5fb23d1220
Owen Devine <owen@devine.dev>
2025-03-30 14:16:31 -0400

    fix: prevent vite dev server from intercepting pages as api calls

--- src/constants/appConstants.ts
+++ src/constants/appConstants.ts
@@ -1,7 +1,7 @@
-export const LOCAL_STORAGE_VERSION = 6;
+export const LOCAL_STORAGE_VERSION = 7;
 
 export const API_BASE_PATH = import.meta.env.DEV
-  ? ""
+  ? "/api"
   : "https://edh-api.devine.dev/v1";
 
 export const AWS_REGION = "us-east-1";
