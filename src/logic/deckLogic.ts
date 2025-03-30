4c88c5530f7be2cdee6c203b6624c63dce0b8f2f
Owen Devine <owen@devine.dev>
2025-03-29 22:21:15 -0400

    chore: cleanup deprecated api logic functions

--- src/logic/deckLogic.ts
+++ src/logic/deckLogic.ts
@@ -1,25 +1,6 @@
-import { AuthUser } from "aws-amplify/auth";
-import { generateClient } from "aws-amplify/data";
 import axios from "axios";
 import { debounce } from "lodash";
 import { useEffect, useRef, useState } from "react";
-
-import {
-  CreateDeckInput,
-  Deck,
-  DeckCategory,
-  DecksByDeckOwnerIdQueryVariables,
-  DeleteDeckInput,
-  UpdateDeckInput,
-} from "@/API";
-import { createDeck, deleteDeck, updateDeck } from "@/graphql/mutations";
-import {
-  decksByDeckOwnerId,
-  listDeckCategories,
-  listDecks,
-} from "@/graphql/queries";
-
-const client = generateClient();
 
 export const colorMap: Record<string, string> = {
   W: "white",
@@ -41,172 +22,8 @@ export const getFullColorNames = (colors: string[]): string => {
     return fullColorNames[0];
   }
 
-  // Join all but the last color with a comma, then append "and" before the last color
+  // join all but the last color with a comma, then append "and" before the last color
   return `${fullColorNames.slice(0, -1).join(", ")} and ${fullColorNames.slice(-1)}`;
-};
-
-export const getAllDecksFn = async (): Promise<Deck[] | null> => {
-  try {
-    let allDecks: Deck[] = [];
-    let nextToken: string | null = null;
-
-    do {
-      const response: any = await client.graphql({
-        query: listDecks,
-        variables: { nextToken },
-      });
-
-      if (
-        response.data &&
-        response.data.listDecks &&
-        response.data.listDecks.items
-      ) {
-        allDecks = [...allDecks, ...response.data.listDecks.items];
-        nextToken = response.data.listDecks.nextToken;
-      } else {
-        nextToken = null;
-      }
-    } while (nextToken);
-
-    return allDecks;
-  } catch (error) {
-    console.error("error fetching decks:", error);
-    return null;
-  }
-};
-
-export const getAllDecksCategoriesFn = async (): Promise<
-  DeckCategory[] | null
-> => {
-  try {
-    let allCategories: DeckCategory[] = [];
-    let nextToken: string | null = null;
-
-    do {
-      const response: any = await client.graphql({
-        query: listDeckCategories,
-        variables: { nextToken },
-      });
-
-      if (
-        response.data &&
-        response.data.listDeckCategories &&
-        response.data.listDeckCategories.items
-      ) {
-        allCategories = [
-          ...allCategories,
-          ...response.data.listDeckCategories.items,
-        ];
-        nextToken = response.data.listDeckCategories.nextToken;
-      } else {
-        nextToken = null;
-      }
-    } while (nextToken);
-
-    return allCategories;
-  } catch (error) {
-    console.error("error fetching deck categories:", error);
-    return null;
-  }
-};
-
-export const getDecksByOwnerFn = async (
-  user: AuthUser,
-): Promise<Deck[] | null> => {
-  // Return early if user object is not valid
-  if (!user || !user.userId) {
-    console.error("invalid or missing user object");
-    return null;
-  }
-
-  const queryVariables: DecksByDeckOwnerIdQueryVariables = {
-    deckOwnerId: user.userId,
-  };
-
-  try {
-    const userDecksResponse = await client.graphql({
-      query: decksByDeckOwnerId,
-      variables: queryVariables,
-    });
-
-    if (userDecksResponse.data && userDecksResponse.data.decksByDeckOwnerId) {
-      return userDecksResponse.data.decksByDeckOwnerId.items as Deck[];
-    }
-    return null;
-  } catch (error) {
-    console.error("Error fetching decks by owner ID:", error);
-    return null;
-  }
-};
-
-export const createDeckFn = async (
-  newDeck: CreateDeckInput,
-): Promise<Deck | null> => {
-  try {
-    const newDeckResponse = await client.graphql({
-      query: createDeck,
-      variables: { input: newDeck },
-    });
-
-    if (newDeckResponse.data && newDeckResponse.data.createDeck) {
-      return newDeckResponse.data.createDeck as Deck;
-    } else {
-      console.error("Failed to create new deck:");
-      return null;
-    }
-  } catch (error) {
-    console.error("Error creating new deck:", error);
-    return null;
-  }
-};
-
-export const updateDeckFn = async (
-  updatedDeck: UpdateDeckInput,
-): Promise<Deck | null> => {
-  try {
-    const updatedDeckResponse = await client.graphql({
-      query: updateDeck,
-      variables: { input: updatedDeck },
-    });
-    if (updatedDeckResponse.data && updatedDeckResponse.data.updateDeck) {
-      return updatedDeckResponse.data.updateDeck as Deck;
-    } else {
-      console.log(`no deck found for ${updatedDeck.id}, or update failed`);
-      return null;
-    }
-  } catch (error) {
-    console.error("error updating deck:", error);
-    return null;
-  }
-};
-
-export const deleteDeckFn = async (deckId: string): Promise<boolean> => {
-  const input: DeleteDeckInput = {
-    id: deckId,
-  };
-
-  try {
-    const deleteDeckResponse = await client.graphql({
-      query: deleteDeck,
-      variables: { input },
-    });
-    if (
-      deleteDeckResponse.data.deleteDeck &&
-      deleteDeckResponse.data.deleteDeck.id
-    ) {
-      console.log(
-        "deck deleted successfully:",
-        deleteDeckResponse.data.deleteDeck,
-      );
-      return true;
-    } else {
-      console.error("Failed to delete the deck:", deleteDeckResponse.errors);
-      return false;
-    }
-  } catch (error) {
-    console.error("Error deleting deck:", error);
-    return false;
-  }
 };
 
 const colorOrder = ["W", "B", "U", "R", "G"];
@@ -220,7 +37,7 @@ export const useCommanderSearch = () => {
   const [commanderSearchTerm, setCommanderSearchTerm] = useState("");
   const [searchResults, setSearchResults] = useState([]);
 
-  // Initialize debouncedSearch with Lodash's debounce
+  // initialize debouncedSearch with Lodash's debounce
   const debouncedSearch = useRef(
     debounce(async (searchTerm: string) => {
       if (searchTerm.length >= 3) {
@@ -237,7 +54,7 @@ export const useCommanderSearch = () => {
   ).current;
 
   useEffect(() => {
-    // This will make sure the effect cleanup only runs once when the component unmounts.
+    // this will make sure the effect cleanup only runs once when the component unmounts.
     return () => {
       debouncedSearch.cancel();
     };
@@ -268,8 +85,8 @@ export const getCardArt = async (
 
     const cardData = await response.json();
 
-    // Scryfall card objects often contain multiple images (e.g., normal, small, etc.)
-    // We will return the URL of the normal size image if available.
+    // scryfall card objects often contain multiple images (e.g., normal, small, etc.)
+    // we will return the URL of the normal size image if available.
     if (cardData.image_uris && cardData.image_uris.normal) {
       return cardData.image_uris.normal;
     } else {
@@ -284,27 +101,27 @@ export const getCardArt = async (
 export function parseDecklist(decklist: string): string[] {
   const parsedDecklist: string[] = [];
 
-  // Split the decklist by newlines
+  // split the decklist by newlines
   const lines = decklist.split("\n");
 
-  // Regular expression to match lines with the format: <quantity> <card name> (optional set/collector number/foil)
+  // regular expression to match lines with the format: <quantity> <card name> (optional set/collector number/foil)
   const regex = /^\s*(\d+)\s+([^(]+)/;
 
   lines.forEach((line) => {
-    // Use regex to extract quantity and card name
+    // use regex to extract quantity and card name
     const match = line.match(regex);
 
     if (match) {
       const quantity = match[1];
       let cardName = match[2].trim();
 
-      // Check if the cardName contains "//" and trim anything after it
+      // check if the cardName contains "//" and trim anything after it
       const splitCardIndex = cardName.indexOf(" // ");
       if (splitCardIndex !== -1) {
         cardName = cardName.substring(0, splitCardIndex).trim();
       }
 
-      // Add to the parsed list in the format: "quantity cardName"
+      // add to the parsed list in the format: "quantity cardName"
       parsedDecklist.push(`${quantity} ${cardName}`);
     }
   });
--- src/logic/index.ts
+++ src/logic/index.ts
@@ -1,6 +1,4 @@
 export * from "./deckLogic";
 export * from "./fetchWithAuth";
-export * from "./matchLogic";
 export * from "./statsLogic";
 export * from "./tableLogic";
-export * from "./userLogic";
