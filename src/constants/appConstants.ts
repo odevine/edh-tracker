f18e7d5f3ea4a978431ad226f0e378c98f58c677
Owen Devine <owen@devine.dev>
2025-03-31 21:57:37 -0400

    fix: correct CORS allowed origins error for api gateway

--- src/constants/appConstants.ts
+++ src/constants/appConstants.ts
@@ -2,7 +2,7 @@ export const LOCAL_STORAGE_VERSION = 7;
 
 export const API_BASE_PATH = import.meta.env.DEV
   ? "/api"
-  : "https://edh-api.devine.dev/v1";
+  : import.meta.env.VITE_API_BASE_PATH;
 
 export const AWS_REGION = "us-east-1";
 export const AWS_COGNITO_CLIENT_ID = "5isdd7vi6i37ovejrnibvh03q2";
