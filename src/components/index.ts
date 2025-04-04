229d9bdf62e281dded8ccfa8a3b7cb49bbc3093f
Owen Devine <owen@devine.dev>
2025-04-04 14:17:50 -0400

    feat: migrate user profile form to a modal for consistency

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,7 +4,6 @@ export * from "./formats";
 export * from "./modals";
 export * from "./pages";
 export * from "./popovers";
-export * from "./profile";
 export * from "./selectors";
 export * from "./tables";
 export * from "./toolbar";
